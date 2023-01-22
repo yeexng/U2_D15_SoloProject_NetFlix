055f1c36bb4c3f8ce579ad22c6444f0e14cb7d06
yeexng <118996751+yeexng@users.noreply.github.com>
2023-01-22 23:17:10 +0000

    Mostly Done... Delete Func need to be fixed

--- backoffice.js
+++ backoffice.js
@@ -1,6 +1,56 @@
 let url = "https://striveschool-api.herokuapp.com/api/movies";
-// img = https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const id = urlParams.get("id");
+console.log(id); //this can be either id or nulll, depend where you click
+//if null => POST
+//if id => PUT
 
+//Edit/PUT Data
+//Auto input the data
+// window.onload = async() =>{
+//   if(id !== null){
+//     let res = await fetch(url + "/" + id,{
+//     headers: {
+//       Authorization:
+//         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5NGRkNWU3MzczODAwMTUzNzQ0MDYiLCJpYXQiOjE2NzQyMTIwODAsImV4cCI6MTY3NTQyMTY4MH0.zN48s058sFaVM0Bbk-uaqF29hWCn4_jd6prsQCHQt-U",
+//     },
+//   })
+//   let {name,description,category,imageUrl} = await res.json()
+//   document.querySelector("#name").value = name
+//   document.querySelector("#description").value = description
+//   document.querySelector("#category").value = category
+//   document.querySelector("#imageUrl").value = imageUrl
+  
+//   }
+// }
+
+
+const editMovie = async (editMovie) =>{
+  const editedMovie = {
+    name: document.querySelector("#name").value,
+    description: document.querySelector("#description").value,
+    category: document.querySelector("#category").value,
+    imageUrl: document.querySelector("#imageUrl").value,
+  }
+  let res = await fetch(url + "/" + id,{
+    method: "PUT",
+    headers: new Headers({
+      "Content-type" : "application/json",
+      Authorization: 
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5NGRkNWU3MzczODAwMTUzNzQ0MDYiLCJpYXQiOjE2NzQyMTIwODAsImV4cCI6MTY3NTQyMTY4MH0.zN48s058sFaVM0Bbk-uaqF29hWCn4_jd6prsQCHQt-U",
+    }),
+    body: JSON.stringify(editedMovie),
+  });
+
+  let changed = await res.json()
+console.log(changed)
+}
+    
+  
+    
+
+//Sending Data
 const sendData = async (movie) => {
   try {
     let res = fetch(url, {
@@ -26,3 +76,4 @@ const submitMovie = () => {
   };
   sendData(movie);
 };
+
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let url = "https://striveschool-api.herokuapp.com/api/movies/";
+let url = "https://striveschool-api.herokuapp.com/api/movies/horror";
 
 window.onload = () => {
   getData();
@@ -15,70 +15,48 @@ const getData = async () => {
     if (res.ok) {
       let data = await res.json();
       console.log(data);
-      renderCategories(data);
+      renderMovie(data);
     }
   } catch (error) {
     console.log(error);
   }
 };
 
-const renderCategories = (genres) => {
-  genres.forEach(renderSingleCategory);
+const renderMovie = (arrayOfMovies) => {
+  let container = document.querySelector(".edit-wrapper");
+  container.innerHTML = "";
+  arrayOfMovies.forEach(singleMovie =>{
+    const {name, description, category, imageUrl, _id} = singleMovie;
+    container.innerHTML += `
+      <div class="card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 p-2 m-2">
+          <img src="${imageUrl}" class="card-img-top" alt="...">
+          <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <p class="card-text">${category}</p>
+              <p class="card-text">${description}</p>
+              <a href="#" class="btn btn-primary">See More...</a>
+              <br>
+              <a href="backoffice.html?id=${_id}" class="btn btn-secondary"><i class="bi bi-pencil-square"></i></a>
+              <button class="btn btn-danger" onclick="deleteEvent("${_id}")"><i class="bi bi-trash"></i></button>
+          </div>
+      </div>
+    
+    `
+  });
 };
 
-const renderSingleCategory = async () => {
-  let container = document.querySelector(".main-wrapper");
-  container.innerHTML += `
-    <div class="container-fluid px-5 pt-5 pb-2">
-    <h2>${genre}</h2>
-    <div id="${genre}Carousel-lg" class="carousel slide d-none d-lg-block" data-interval="false">
-        <a class="carousel-control-prev" href="#${genre}Carousel-lg" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#${genre}Carousel-lg" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-        <div class="carousel-inner">
-        </div>
-    </div>
-    <div id="${genre}Carousel-md" class="carousel slide d-none d-md-block d-lg-none" data-interval="false">
-        <div class="carousel-inner">
-        </div>
-        <a class="carousel-control-prev" href="#${genre}Carousel-md" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#${genre}Carousel-md" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-    <div id="${genre}Carousel-sm" class="carousel slide d-block d-md-none" data-interval="false">
-        <div class="carousel-inner">
-        </div>
-        <a class="carousel-control-prev" href="#${genre}Carousel-sm" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#${genre}Carousel-sm" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-</div>
-      `;
-  await getMovies(genre);
-};
-
-const getMovies = async (genre) => {
-  try {
-    const res = await fetch(url + genre, {
-      headers: {
+const deleteEvent = async(idToDelete) => {
+  try { 
+    let res = await fetch (url + "/" + idToDelete,{
+      method: "DELETE",
+      headers: new Headers({
+        "Content-Type": "application/json",
         Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5NGRkNWU3MzczODAwMTUzNzQ0MDYiLCJpYXQiOjE2NzQyMTIwODAsImV4cCI6MTY3NTQyMTY4MH0.zN48s058sFaVM0Bbk-uaqF29hWCn4_jd6prsQCHQt-U",
-      },
-    });
-  } catch (error) {}
-};
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5NGRkNWU3MzczODAwMTUzNzQ0MDYiLCJpYXQiOjE2NzQyMTIwODAsImV4cCI6MTY3NTQyMTY4MH0.zN48s058sFaVM0Bbk-uaqF29hWCn4_jd6prsQCHQt-U",
+      })
+    })
+    let deletedMovie = await res.json()
+    
+  } catch (error) {
+  }
+}
